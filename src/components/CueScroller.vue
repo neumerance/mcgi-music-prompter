7dd5b537cdfe672e5250fc79bc8f0c99cc2a7de6
jonjon <neumerance@live.com>
2023-02-14 02:59:06 +0800

    add auto scroller

--- src/components/CueScroller.vue
+++ src/components/CueScroller.vue
@@ -1,26 +1,46 @@
 <script>
   import lyrics from "@/assets/images/lyrics.jpg";
+  import Slider from '@vueform/slider'
 
   export default {
+    components: {
+      Slider
+    },
     data() {
       return {
-        lyrics: lyrics
+        lyrics: lyrics,
+        scrollSpeed: 0
       }
     }
   }
 </script>
 
 <template>
-  <div class="cue-scroll">
-    <img class="cue-scroll__lyrics" :src="lyrics" />
+  <div>
+    <div class="cue-scroll">
+      <vue3-auto-scroll hide-scroll-bar :control="false" :speed="scrollSpeed">
+        <img class="cue-scroll__lyrics" :src="lyrics" />
+      </vue3-auto-scroll>
+    </div>
+    <div class="cue-controls">
+      <Slider v-model="scrollSpeed" :min="0" :max="5" :step=".1" :tooltips="false" />
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
   .cue-scroll {
+    height: 97vh;
+    overflow: hidden;
+
     &__lyrics {
       display: block;
       width: 100%;
     }
+  }
+
+  .slider-target {
+    --slider-handle-width: 32px;
+    --slider-handle-height: 32px;
   }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import Vue3AutoScroll from "vue3-auto-scroll";
 
 import '@/assets/main.scss'
 
@@ -8,5 +9,6 @@ import '@/assets/main.scss'
 const app = createApp(App)
 
 app.use(router)
+app.use(Vue3AutoScroll)
 
 app.mount('#app')
